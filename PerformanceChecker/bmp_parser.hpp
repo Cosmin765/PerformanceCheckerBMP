1bb90c03b2adfc565628d90763c768f7bfbfd042
reikstar <reikstar1@gmail.com>
2024-01-04 15:07:13 +0200

    first commit

--- PerformanceChecker/bmp_parser.hpp
+++ PerformanceChecker/bmp_parser.hpp
@@ -1,7 +1,36 @@
-#include <stdio.h>
+#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <string>
 
-// de pus in proiectul asta toate chestiile pentru parsarea bmp-urilor (nu toate intr-un singur fisier :)) )
+std::vector<BYTE> getPixelArrayOffset(HANDLE hFile){
 
-void process() {
-	printf("Hello there!\n");
+	std::vector<BYTE> bytes (4);
+	DWORD bytesToRead = 4;
+	DWORD bytesRead;
+	OVERLAPPED ol{};
+	
+	ol.Offset = 10; // the offset where the pixel array starts is located at the 10th byte
+
+	if (!ReadFile(hFile, bytes.data(), bytesToRead, &bytesRead, &ol)){
+		DWORD error = GetLastError();
+		throw std::runtime_error ("ReadFile error " + std::to_string(error));
+
+	}
+	
+	return bytes;
+
 }
+
+int bytesToInt(const std::vector<BYTE>& bytes) {
+    
+    int result = 0;
+    for (int i = 0; i < bytes.size(); i++) {
+        result |= static_cast<int>(bytes[i]) << (i * 8); // BMP file header uses little endian format
+    }
+    
+    return result;
+}
+
+
+
